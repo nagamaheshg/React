0689e4376ea142b8bdeefe58b245e6d401de4bb7
Naga Mahesh Gatta <mahesh@kali.mahesh.com>
2024-11-15 15:31:35 +0530

    Completed Project Structure

--- 03-Advanced-React/src/App.jsx
+++ 03-Advanced-React/src/App.jsx
@@ -1,9 +1,17 @@
-import Starter from './tutorial/02-useEffect/starter/05-cleanup-function';
+// import Navbar from './tutorial/04-project-structure/starter/Navbar/';
+import Example from './tutorial/04-project-structure/starter/Example';
+//import {Home, About} from './tutorial/04-project-structure/starter/Pages';
+
+
+
 
 function App() {
 	return (
 		<div className='container'>
-			<Starter />
+			{/* <Navbar /> */}
+			{/* <Home/>
+            <About /> */}
+            <Example/>
 		</div>
 	);
 }
---
+++ 03-Advanced-React/src/tutorial/04-project-structure/starter/Example/FirstComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FirstComponent = () => {
+  return (
+    <div>
+      First Component
+    </div>
+  )
+}
+
+export default FirstComponent
